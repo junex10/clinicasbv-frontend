f16ea5f703dff5dd4992de77d6a8bbf7a0c09f82
jherrada10 <43711689+junex10@users.noreply.github.com>
2022-03-31 22:41:38 -0400

    *

--- src/app/web/dashboard/dashboard.module.ts
+++ src/app/web/dashboard/dashboard.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
+// Login
+import { LoginComponent } from './login/login.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'prefix',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
+    RouterModule.forChild(routes),
     CommonModule
-  ]
+  ],
 })
 export class DashboardModule { }
