d95f881ce20a3151329887bd17b6e071fd679ccd
jherrada10 <43711689+junex10@users.noreply.github.com>
2022-05-21 18:28:33 -0400

    Datatable

--- src/app/shared/datatable/datatable.component.ts
+++ src/app/shared/datatable/datatable.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, Input, ViewEncapsulation, OnChanges, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, Output, EventEmitter, SimpleChange } from '@angular/core';
 import { Constants } from 'src/app/shared';
 
 @Component({
-  selector: 'app-datatable',
+  selector: 'app-table',
   templateUrl: './datatable.component.html',
   styleUrls: ['./datatable.component.css'],
   encapsulation: ViewEncapsulation.Emulated
@@ -24,36 +24,27 @@ export class DatatableComponent implements OnChanges {
   constructor() { }
 
   ngOnChanges(): void {
-    this.dtOptions = {
-      ...this.options,
-      pageLength: Constants.PER_PAGE_WEB,
-      responsive: true,
-      pagingType: 'numbers',
-      language: {
-        processing: "Procesando...",
-        search: "Buscar:",
-        lengthMenu: "Mostrar _MENU_ Elementos",
-        info: "Mostrando desde _START_ al _END_ de _TOTAL_ elementos",
-        infoEmpty: "Mostrando ningún elemento.",
-        infoFiltered: "(filtrado _MAX_ elementos total)",
-        infoPostFix: "",
-        loadingRecords: "Cargando registros...",
-        zeroRecords: "No se encontraron registros",
-        emptyTable: "No hay datos disponibles en la tabla",
-       /* paginate: {
-          first: "Primero",
-          previous: "Anterior",
-          next: "Siguiente",
-          last: "Último"
-        },*/
-        aria: {
-          sortAscending: ": Activar para ordenar la tabla en orden ascendente",
-          sortDescending: ": Activar para ordenar la tabla en orden descendente"
-        },
-        ...this.language
-      }
-    };
-
+    setTimeout(() => {
+      this.dtOptions = {
+        ...this.options,
+        pageLength: Constants.PER_PAGE_WEB,
+        responsive: true,
+        pagingType: 'numbers',
+        language: {
+          processing: "Procesando...",
+          search: "Buscar:",
+          lengthMenu: "Mostrar _MENU_ Elementos",
+          info: "Mostrando desde _START_ al _END_ de _TOTAL_ elementos",
+          infoEmpty: "Mostrando ningún elemento.",
+          infoFiltered: "(filtrado _MAX_ elementos total)",
+          infoPostFix: "",
+          loadingRecords: "Cargando registros...",
+          zeroRecords: "No se encontraron registros",
+          emptyTable: "No hay datos disponibles en la tabla",
+          ...this.language
+        }
+      };
+    }, 600)
   }
 
   pageChange = (page: any) => {
